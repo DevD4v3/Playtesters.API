5162d54691f110863f7cc4277c25c68a0a68ec39
MrDave1999 <daveseva2010@hotmail.es>
2025-11-16 16:29:59 -0500

    cleanup: remove IsActive property

--- src/Entities/Tester.cs
+++ src/Entities/Tester.cs
@@ -5,6 +5,5 @@ public class Tester
     public int Id { get; set; }
     public string UserName { get; set; } = string.Empty;
     public string ApiKey { get; set; } = Guid.NewGuid().ToString();
-    public bool IsActive { get; set; } = true;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
